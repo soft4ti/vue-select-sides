361300594a86cdd6378c9d531d013811206c4a97
Gabriel Caixeta Silva <gabriel@soft4.com.br>
2024-10-09 18:30:32 -0300

    remoção do console

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default {
   install(app, options) {
     app.config.globalProperties.defaultOptions =
       options && options.constructor.name === "Object" ? options : {};
+
     app.component("vueSelectSides", vueSelectSides);
-    console.trace("install component: src/index.js");
   },
 };
